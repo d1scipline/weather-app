6e5163b78bd111426a1b7f1bd29394941bfb8985
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-02-08 01:18:43 +0300

    Add basic function to call the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const API_KEY = "QMZBY6GXY32HGADG5X8S568GL";
+
+async function getWeather(city) {
+  let rawData = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=days%2Chours%2Ccurrent%2Calerts&key=${API_KEY}&contentType=json`,
+  );
+  let weatherData = await rawData.json();
+  console.log(weatherData);
+}
+
+getWeather("Ankara");
