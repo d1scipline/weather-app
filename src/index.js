f0ea24557b6a0803c42770bf252812c048e3e03c
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-02-08 22:20:42 +0300

    Gather assets and add search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import "./styles.css";
 
 const API_KEY = "QMZBY6GXY32HGADG5X8S568GL";
+const searchButton = document.getElementById("city-search-btn");
+const cityInput = document.getElementById("city-input");
 
 async function getWeather(city) {
-  let rawData = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=days%2Chours%2Ccurrent%2Calerts&key=${API_KEY}&contentType=json`,
-  );
-  let weatherData = await rawData.json();
-  console.log(weatherData);
+  try {
+    let rawData = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=days%2Chours%2Ccurrent%2Calerts&key=${API_KEY}&contentType=json&iconSet=icons1`,
+    );
+    let weatherData = await rawData.json();
+    let weatherDataObject = {};
+    weatherDataObject["city"] = weatherData.resolvedAddress;
+    weatherDataObject["condition"] = weatherData.currentConditions.conditions;
+    weatherDataObject["icon"] = weatherData.currentConditions.icon;
+    weatherDataObject["temp"] = weatherData.currentConditions.temp;
+    weatherDataObject["humidity"] = weatherData.currentConditions.humidity;
+    weatherDataObject["pressure"] = weatherData.currentConditions.pressure;
+    weatherDataObject["feels_like"] = weatherData.currentConditions.feelslike;
+    weatherDataObject["wind"] = weatherData.currentConditions.windspeed;
+    console.log(weatherDataObject);
+    console.log(weatherData);
+  } catch {
+    console.log("Error");
+  }
 }
 
-getWeather("Ankara");
+searchButton.addEventListener("click", () => {
+  getWeather(cityInput.value);
+  cityInput.value = "";
+});
